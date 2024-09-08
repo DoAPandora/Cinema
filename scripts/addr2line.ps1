3382b64b32193a6d852932493b971365d6bca33e
kodenamekrak <kyelodge2005@gmail.com>
2024-09-08 09:45:36 +0100

    Update scripts

--- scripts/addr2line.ps1
+++ scripts/addr2line.ps1
@@ -1,11 +1,11 @@
-param($p1)
+param($p1, $p2)
 
 if(Test-Path "./ndkpath.txt") {
-    ndkpath = Get-Content "./ndkpath.txt"
+    $ndkpath = Get-Content "./ndkpath.txt"
 }
 else
 {
     $ndkpath = $env:ANDROID_NDK_HOME
 }
 $child = Get-ChildItem -Path $ndkpath/toolchains/llvm/prebuilt -Name
-& $ndkpath/toolchains/llvm/prebuilt/$child/bin/llvm-addr2line -e ./build/debug/libcinema.so $p1
+& $ndkpath/toolchains/llvm/prebuilt/$child/bin/llvm-addr2line -e ./build/debug/$p1 $p2
--- scripts/restart-game.ps1
+++ scripts/restart-game.ps1
@@ -1,2 +1,3 @@
 adb shell am force-stop com.beatgames.beatsaber
+Start-Sleep 0.5
 adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
