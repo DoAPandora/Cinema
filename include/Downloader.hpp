4daea648ab3b2e0d00df2f919b18b0fd09ed2dce
kodenamekrak <kyelodge2005@gmail.com>
2024-10-19 14:02:42 +0100

    Remove old unused downloader

--- include/Downloader.hpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include "custom-types/shared/coroutine.hpp"
-
-namespace Cinema::Downloader {
-    bool DownloadVideo(std::string_view url, std::function<void(float)> status = nullptr);
-
-    custom_types::Helpers::Coroutine DownloadThumbnail(std::string_view id, std::function<void()> onFinished);
-}
