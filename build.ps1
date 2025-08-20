43b8d2cc10ce9793d8935ac6d8bcfebb8818e13e
VoicesV2 <100880439+DoAPandora@users.noreply.github.com>
2025-08-20 19:47:52 -0400

    Add files via upload

---
+++ build.ps1
@@ -0,0 +1,36 @@
+
+Param(
+    [Parameter(Mandatory=$false)]
+    [Switch]$clean,
+    [Parameter(Mandatory=$false)]
+    [Switch]$release
+)
+
+# if user specified clean, remove all build files
+if ($clean.IsPresent)
+{
+    if (Test-Path -Path "build")
+    {
+        remove-item build -R
+    }
+}
+
+$buildType = "Debug"
+if ($release.IsPresent) {
+    $buildType = "RelWithDebInfo"
+    echo "Building release"
+} else {
+    echo "Building debug"
+}
+
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+
+if (($clean.IsPresent) -or (-not (Test-Path -Path "build")))
+{
+    $out = new-item -Path build -ItemType Directory
+}
+
+& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="$buildType" . -B build 
+& cmake --build ./build
+$ExitCode = $LastExitCode
+exit $ExitCode
---
+++ createqmod.ps1
@@ -0,0 +1,106 @@
+
+Param(
+    [String] $qmodname="",
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $clean,
+    
+    [Parameter(Mandatory=$false)]
+    [Switch]$release,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $help
+)
+
+if ($help -eq $true) {
+    Write-Output "`"BuildQmod <qmodName>`" - Copiles your mod into a `".so`" or a `".a`" library"
+    Write-Output "`n-- Parameters --`n"
+    Write-Output "qmodName `t The file name of your qmod"
+
+    Write-Output "`n-- Arguments --`n"
+
+    Write-Output "-Clean `t`t Performs a clean build on both your library and the qmod"
+
+    exit
+}
+
+if ($qmodName -eq "")
+{
+    Write-Output "Give a proper qmod name and try again"
+    exit
+}
+
+& $PSScriptRoot/build.ps1 -clean:$clean -release:$release
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Output "Failed to build, exiting..."
+    exit $LASTEXITCODE
+}
+
+Write-Output "Creating qmod from mod.json"
+
+$schemaUrl = "https://raw.githubusercontent.com/Lauriethefish/QuestPatcher.QMod/main/QuestPatcher.QMod/Resources/qmod.schema.json"
+Invoke-WebRequest $schemaUrl -OutFile ./mod.schema.json
+
+$mod = "./mod.json"
+$schema = "./mod.schema.json"
+$modJsonRaw = Get-Content $mod -Raw
+$modJson = $modJsonRaw | ConvertFrom-Json
+$modSchemaRaw = Get-Content $schema -Raw
+
+Remove-Item ./mod.schema.json
+
+Write-Output "Validating mod.json..."
+if(!($modJsonRaw | Test-Json -Schema $modSchemaRaw)) {
+    exit
+}
+
+$filelist = @($mod)
+
+$cover = "./" + $modJson.coverImage
+if ((-not ($cover -eq "./")) -and (Test-Path $cover))
+{
+    $filelist += ,$cover
+}
+
+foreach ($mod in $modJson.modFiles)
+{
+    $path = "./build/" + $mod
+    if (-not (Test-Path $path))
+    {
+        $path = "./extern/libs/" + $mod
+    }
+    $filelist += $path
+}
+
+foreach ($mod in $modJson.lateModFiles)
+{
+    $path = "./build/" + $mod
+    if (-not (Test-Path $path))
+    {
+        $path = "./extern/libs/" + $mod
+    }
+    $filelist += $path
+}
+
+foreach ($lib in $modJson.libraryFiles)
+{
+    $path = "./extern/libs/" + $lib
+    if (-not (Test-Path $path))
+    {
+        $path = "./build/" + $lib
+    }
+    $filelist += $path
+}
+
+$zip = $qmodName + ".zip"
+$qmod = $qmodName + ".qmod"
+
+if ((-not ($clean.IsPresent)) -and (Test-Path $qmod))
+{
+    Write-Output "Making Clean Qmod"
+    Move-Item $qmod $zip -Force
+}
+
+Compress-Archive -Path $filelist -DestinationPath $zip -Update
+Move-Item $zip $qmod -Force
